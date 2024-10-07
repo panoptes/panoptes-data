4688e6bd034b5c54d469f05a68364cbf29ffb3be
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-10-07 11:06:18 -1000

    Do better path matching for image url for new scheme.

--- src/panoptes/data/settings.py
+++ src/panoptes/data/settings.py
@@ -16,14 +16,14 @@ class CloudSettings(BaseSettings):
 # with a PANOPTES unit, including the optional "field name".
 PATH_MATCHER: Pattern[str] = re.compile(
     r"""^
-                                    (?P<pre_info>.*)?                       # Anything before unit_id
-                                    (?P<unit_id>PAN\d{3})                   # unit_id   - PAN + 3 digits
-                                    /?(?P<field_name>.*)?                   # Legacy field name - any                 
-                                                   
-                                    /(?P<camera_id>[a-gA-G0-9]{6})          # camera_id - 6 digits
-                                    /(?P<sequence_time>[0-9]{8}T[0-9]{6})   # Observation start time
-                                    /(?P<image_time>[0-9]{8}T[0-9]{6})      # Image start time
-                                    (?P<post_info>.*)?                      # Anything after (file ext)
-                                    $""",
+    (?P<pre_info>.*)?                                                   # Anything before unit_id
+    (?P<unit_id>PAN\d{3})                                               # unit_id   - PAN + 3 digits
+    /?(?P<field_name>.*)?                                               # Legacy field name - any
+    [/_](?P<camera_id>[a-gA-G0-9]{6})                                   # camera_id - 6 digits
+    [/_](?P<sequence_time>[0-9]{8}T[0-9]{6})                            # Observation start time
+    [/_](?P<full_image_id>PAN\d{3}_[a-gA-G0-9]{6}_[0-9]{8}T[0-9]{6}_)?  # Observation full image id
+    [/_]?(?P<image_time>[0-9]{8}T[0-9]{6})                              # Image start time
+    (?P<post_info>.*)?                                                  # Anything after (file ext)
+    $""",
     re.VERBOSE
 )
